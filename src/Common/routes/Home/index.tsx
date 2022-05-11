690c191cfdbb651939588eb83a4df5bfb6a95478
vara35 <kadiyalavaraprasad35@gmail.com>
2022-05-11 19:27:59 +0530

    add code to git

--- src/Common/routes/Home/index.tsx
+++ src/Common/routes/Home/index.tsx
@@ -1,35 +1,55 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { observer } from 'mobx-react'
-import 'twin.macro'
-import { Button, LensProvider } from '@ib/nebula-system'
+// import { Link } from 'react-router-dom'
+// import { observer } from 'mobx-react'
+// import 'twin.macro'
+// import { Button, LensProvider } from '@ib/nebula-system'
 
-import { tailwindContainerCSS, TailwindContainer } from './styledComponents'
-@observer
-class Home extends Component {
-   render(): React.ReactElement {
-      return (
-         <div>
-            <h2>Home</h2>
-            <Link to='/counter'>
-               <p>Counter</p>
-            </Link>
-            <TailwindContainer
-               css={[
-                  tailwindContainerCSS
-                  /* Combine regular css and tailwind styles within backticks */
-               ]}
-            >
-               <div tw='flex flex-col justify-center h-full space-y-5'>
-                  <p className='bg-green-500'>Tailwind Column Item</p>
-                  <p>Tailwind Column Item</p>
-               </div>
-            </TailwindContainer>
-            <LensProvider>
-               <Button variant='positive'>Positive</Button>
-            </LensProvider>
-         </div>
-      )
+import {
+   tailwindContainerCSS,
+   TailwindContainer,
+   MainContainer
+} from './styledComponents'
+// @observer
+// class Home extends Component {
+//    render(): React.ReactElement {
+//       return (
+//          <div>
+//             <h2>Hello</h2>
+//             <Link to='/counter'>
+//                <p>Counter</p>
+//             </Link>
+//             <TailwindContainer
+//                css={[
+//                   tailwindContainerCSS
+//                   /* Combine regular css and tailwind styles within backticks */
+//                ]}
+//             >
+//                <div tw='flex flex-col justify-center h-full space-y-5'>
+//                   <p className='bg-green-500'>Tailwind Column Item</p>
+//                   <p>Tailwind Column Item</p>
+//                </div>
+//             </TailwindContainer>
+//             <LensProvider>
+//                <Button variant='positive'>Positive</Button>
+//             </LensProvider>
+//          </div>
+//       )
+//    }
+// }
+
+// export default Home
+
+type MyProps = {
+   // using `interface` is also ok
+   message: string
+}
+type MyState = {
+   count: number // like this
+}
+class Home extends React.Component<MyProps, MyState> {
+   state = { count: 0 }
+   render() {
+      return <MainContainer>Hello</MainContainer>
    }
 }
 
--- src/Common/routes/Home/styledComponents.tsx
+++ src/Common/routes/Home/styledComponents.tsx
@@ -2,6 +2,10 @@ import styled, { css } from 'styled-components'
 
 import tw from 'twin.macro'
 
+export const MainContainer = styled.div`
+   ${tw`bg-black h-screen`}
+`
+
 export const TailwindContainer = styled.div`
    ${tw`flex flex-col items-center justify-center md:flex-row text-xl `}
 `
